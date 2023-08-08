939d0a38a12a87422b5dcc164169911af17728f8
marekgarbowski <marekgarbowski@proton.me>
2023-08-08 23:24:39 +0200

    Restore to origin code

--- article_crawler.py
+++ article_crawler.py
@@ -1,23 +1,41 @@
-import os
+import multiprocessing
 import re
-import random
 import requests
 import justext
-import multiprocessing
+import random
+import os
+
 from multiprocessing import Manager
 from itertools import repeat
 
 # CONFIG
-TXT_FILES_PATH = 'your-directory-path/'
-TXT_URLS = 'txt-file-with-urls'
-MIN_LENGTH = 2000
-PROCESSES = 3
+TXT_FILES_PATH = 'your-directory-path/'  # remember about the '/', has to be created before starting the script
+TXT_URLS = 'txt-file-with-urls'  # provide directory to .txt containing URLs
+MIN_LENGTH = 2000  # minimal number of characters for a text to be collected
+PROCESSES = 3  # number of processes, from 1 to os.cpu_count()
+
+
 ###
 
-
-def process_item(item, file_count, file_total):
+def process_item(item, counter, total_files):
+    counter.value += 1
+    ccc = random.randrange(1, 1000000000)
     txt = ''
     response = requests.get(item)
+
+    """
+    Tool used to analyze and extract text from websites is a library called 'JusText' by Jan Pomikalek. 
+
+    Be sure to experiment with different URLs from set manually and fine-tune the params provided by the author. 
+    Ones provided below are the results of experiments on a set of URLs from a forum type website. Most likely they
+    will not fit to your use-case.
+
+    Fine-tuning should be done once per website/dataset.
+
+    To try different possibilites and sets of params, visit:
+    https://nlp.fi.muni.cz/projects/justext/
+    """
+
     paragraphs = justext.justext(response.content, justext.get_stoplist("Polish"),
                                  max_heading_distance=150,
                                  length_low=10,
@@ -31,17 +49,13 @@ def process_item(item, file_count, file_total):
             txt += paragraph.text + " "
 
     txt = re.sub(r"\n{2,}", "\n", txt)
-
-    with file_count.get_lock():
-        file_count.value += 1
-        print("Processing file: {}, {}/{}, {:.2f}%".format(item, file_count.value, file_total,
-                                                           (file_count.value / file_total * 100)))
+    print("Processing file: " + item, counter.value, "/", total_files, round((counter.value / total_files * 100), 2),
+          "%")
 
     if len(txt) > MIN_LENGTH:
         try:
-            with open(os.path.join(TXT_FILES_PATH, f"{random.randint(1, 1_000_000_000)}.txt"), 'w',
-                      encoding="utf-8") as file_handle:
-                file_handle.write(txt)
+            with open(f"{TXT_FILES_PATH}" + str(ccc) + ".txt", 'w', encoding="utf-8") as f:
+                f.write(txt)
         except Exception as e:
             print(e)
 
@@ -56,4 +70,8 @@ if __name__ == '__main__':
     total_files = len(txt_files)
 
     with multiprocessing.Pool(processes=PROCESSES) as pool:
-        pool.starmap(process_item, zip(txt_files, repeat(counter), repeat(total_files)), chunksize=8)
+        results = pool.starmap(process_item,
+                               zip(txt_files,
+                                   repeat(counter),
+                                   repeat(total_files)),
+                               chunksize=8)
--- crawler_custom.py
+++ crawler_custom.py
@@ -4,14 +4,20 @@ import pathlib
 import time
 import urllib.parse
 import httpx
+import re
 import os
+
 from typing import Callable, Iterable
+from urllib.parse import urlparse
 
 # CONFIG
-WORKERS = os.cpu_count()
-LIMIT = 1000000
-CRAWLER_LIMIT = 50000
-FILE = 'custom.txt'
+WORKERS = os.cpu_count()  # depending on available computing power, from 1 to os.cpu_count()
+LIMIT = 10000000  # maximum number of URLs to crawl
+CRAWLER_LIMIT = 5000000  # maximum number of URLs cralwer can process
+FILE = 'custom.txt'  # source file with domain/set of domains
+
+
+###
 
 
 class UrlFilterer:
@@ -19,67 +25,125 @@ class UrlFilterer:
             self,
             allowed_domains: set[str] | None = None,
             allowed_schemes: set[str] | None = None,
-            allowed_filetypes: set[str] | None = None
+            allowed_filetypes: set[str] | None = None,
+            restricted_urls: set[str] | None = None
     ):
         self.allowed_domains = allowed_domains
         self.allowed_schemes = allowed_schemes
         self.allowed_filetypes = allowed_filetypes
         self.restricted_urls = [
             "web.archive.org", "plugins", ":8080", "moodle", "kalendarz",
-            "password", "mobile", "query", "calendar", "ajax", "Zaloguj",
-            "reddit.", "source=", "rozmiar=", "ssid=", "f_ov", "Facebook=",
-            "cookies", "add", "cart", "comment", "reply", "en_US", "/login",
-            "/logowanie", "producer_", "register", "orderby", "tumblr.",
-            "redirect", "linkedin.", "facebook.", "instagram.", "youtube.",
-            "twitter.", "whatsapp.", "pinterest.", "login.", "google.",
-            "wykop.", "drukuj/", "pliki/"
+            "password", "mobile", "query", "calendar",
+            "ajax", "Zaloguj", "reddit.", "source=", "rozmiar=",
+            "ssid=", "f_ov", "Facebook=", "cookies", "add", "cart", "comment",
+            "reply", "en_US", "/login", "/logowanie", "producer_", "register",
+            "orderby", "tumblr.", "redirect", "linkedin.", "facebook.",
+            "instagram.", "youtube.", "twitter.", "whatsapp.", "pinterest.",
+            "login.", "google.", "wykop.", "drukuj/", "pliki/"
         ]
 
-    def filter_url(self, base: str, filtered_link: str) -> str | None:
-        filtered_link = urllib.parse.urljoin(base, filtered_link)
-        filtered_link, _ = urllib.parse.urldefrag(filtered_link)
-        parsed = urllib.parse.urlparse(filtered_link)
+    def filter_url(self, base: str, url: str) -> str | None:
+        # base_count = url.count(base)
+        # If the base URL is present more than once, remove it
+        # if base_count > 1:
+        # 	url = url.replace(base, '', 1)
 
-        if ((self.allowed_schemes is not None and parsed.scheme not in self.allowed_schemes)
-                or any(substring in filtered_link for substring in self.restricted_urls)
-                or any(pattern in filtered_link.lower() for pattern in
-                       ["login", "signin", "auth", "logon", "signon", "logowanie", "rejestracja"])
-                or (self.allowed_filetypes is not None and pathlib.Path(
-                    parsed.path).suffix not in self.allowed_filetypes)):
+        url = urllib.parse.urljoin(base, url)
+        url, _ = urllib.parse.urldefrag(url)
+        parsed = urllib.parse.urlparse(url)
+        # segments = parsed.path.split('/')
+
+        # # Remove duplicates
+        # unique_segments = []
+        # seen_segments = set()
+        # for segment in segments:
+        # 	if segment not in seen_segments:
+        # 		unique_segments.append(segment)
+        # 		seen_segments.add(segment)
+
+        # # Reconstruct the URL
+        # parsed = parsed._replace(path='/'.join(unique_segments))
+        # url = urllib.parse.urlunparse(parsed)
+
+        if (self.allowed_schemes is not None
+                and parsed.scheme not in self.allowed_schemes):
             return None
 
-        return filtered_link
+        if any(substring in url for substring in self.restricted_urls):
+            return None
+
+        if self.allowed_domains is not None and self.allowed_domains not in url:
+            return None
+
+        login_patterns = ["login", "signin", "auth", "logon", "signon", "logowanie", "rejestracja"]
+
+        if any(pattern in url.lower() for pattern in login_patterns):
+            return None
+
+        # Check for repeating path segments
+        # path_segments = parsed.path.strip("/").split("/")
+        # for i, segment in enumerate(path_segments[:-1]):
+        # 	if segment == path_segments[i + 1]:
+        # 		return None
+
+        # Check for too many unique path segments
+        # unique_segments = len(set(path_segments))
+        # if unique_segments > 10:  # Adjust the threshold as needed
+        # 	return None
+
+        # if sum([parsed.path.count(char) + parsed.query.count(char) for char in "':;,.'"]) > 2:
+        # 	return None
+
+        # Check for long random strings (hashes)
+        # if re.search(r'[a-zA-Z0-9_-]{20,}', url):
+        # 	return None
+
+        ext = pathlib.Path(parsed.path).suffix
+        if (self.allowed_filetypes is not None
+                and ext not in self.allowed_filetypes):
+            return None
+        return url
 
 
 class UrlParser(html.parser.HTMLParser):
-    def __init__(self, base: str, filter_url: Callable[[str, str], str | None]):
+    def __init__(
+            self,
+            base: str,
+            filter_url: Callable[[str, str], str | None]
+    ):
         super().__init__()
         self.base = base
         self.filter_url = filter_url
         self.found_links = set()
 
     def handle_starttag(self, tag: str, attrs):
+
         if tag != "a":
             return
-        for attr, link in attrs:
-            if attr == "href" and (link := self.filter_url(self.base, link)) is not None:
-                self.found_links.add(link)
+
+        for attr, url in attrs:
+            if attr != "href":
+                continue
+
+            if (url := self.filter_url(self.base, url)) is not None:
+                self.found_links.add(url)
 
 
 class Crawler:
     def __init__(
             self,
             client: httpx.AsyncClient(),
-            entry_urls: Iterable[str],
+            urls: Iterable[str],
             filter_url: Callable[[str, str], str | None],
             workers: int = WORKERS,
-            limit: int = LIMIT
+            limit: int = CRAWLER_LIMIT,
     ):
         self.client = client
-        self.start_urls = set(entry_urls)
+        self.start_urls = set(urls)
         self.todo = asyncio.Queue()
         self.seen = set()
         self.done = set()
+
         self.filter_url = filter_url
         self.num_workers = workers
         self.limit = limit
@@ -87,8 +151,12 @@ class Crawler:
 
     async def run(self):
         await self.on_found_links(self.start_urls)
-        workers = [asyncio.create_task(self.worker()) for _ in range(self.num_workers)]
+        workers = [
+            asyncio.create_task(self.worker())
+            for _ in range(self.num_workers)
+        ]
         await self.todo.join()
+
         for worker in workers:
             worker.cancel()
 
@@ -96,70 +164,105 @@ class Crawler:
         while True:
             try:
                 await self.process_one()
-            except asyncio.CancelledError:
+            except asyncio.CancelledError as e:
                 return
 
     async def process_one(self):
-        page = await self.todo.get()
+        url = await self.todo.get()
         try:
-            await self.crawl(page)
+            await self.crawl(url)
+        except Exception as exc:
+            # retry handling here...
+            pass
         finally:
             self.todo.task_done()
 
-    async def crawl(self, website: str):
+    async def crawl(self, url: str):
+
+        # rate limit here...
         await asyncio.sleep(.1)
-        response = await self.client.get(website, follow_redirects=True)
-        found_links = await self.parse_links(base=str(response.url), text=response.text, )
+
+        response = await self.client.get(url, follow_redirects=True)
+        found_links = await self.parse_links(
+            base=str(response.url),
+            text=response.text,
+        )
+
         await self.on_found_links(found_links)
-        self.done.add(website.lower())
+
+        self.done.add(url.lower())
 
     async def parse_links(self, base: str, text: str) -> set[str]:
         parser = UrlParser(base, self.filter_url)
         parser.feed(text)
         return parser.found_links
 
-    async def on_found_links(self, found_links: set[str]):
-        new = found_links - self.seen
+    async def on_found_links(self, urls: set[str]):
+        new = urls - self.seen
         self.seen.update(new)
-        for i, found_link in enumerate(new):
-            if len(found_link) <= 256 and not found_link.count(" "):
-                await self.put_todo(found_link)
 
-    async def put_todo(self, item: str):
-        if self.total < self.limit:
-            self.total += 1
-            await self.todo.put(item)
+        # await save to database or file here...
+
+        for i, url in enumerate(new):
+            if len(url) > 256 or url.count(" "):
+                pass
+            else:
+                print(i, url)
+                await self.put_todo(url)
+
+    async def put_todo(self, url: str):
+        if self.total >= self.limit:
+            return
+        self.total += 1
+        print("total: ", self.total)
+        await self.todo.put(url)
 
 
-async def main(input_url):
-    url_domain = urllib.parse.urlparse(input_url).netloc.replace("www.", "")
+async def main(url):
+    url_domain = urllib.parse.urlparse(url).netloc
+    url_domain = url_domain.replace("www.", "")
+    print(url_domain)
     filterer = UrlFilterer(
         allowed_domains=url_domain,
         allowed_schemes={"http", "https"},
         allowed_filetypes={".html", ".htm", ".php", ".asp", ".aspx", ".jsp", ".cgi", ""}
     )
 
+    start = time.perf_counter()
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/77.0.3865.120 Safari/537.36',
         "Accept-Encoding": "gzip, deflate",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Connection": "keep-alive"
-    }
+        "Connection": "keep-alive"}
     async with httpx.AsyncClient(headers=headers) as client:
-        crawler = Crawler(client=client, entry_urls=[input_url], filter_url=filterer.filter_url)
+        crawler = Crawler(
+            client=client,
+            urls=[url],
+            filter_url=filterer.filter_url,
+            workers=WORKERS,
+            limit=LIMIT,
+        )
         await crawler.run()
-        seen = sorted(crawler.seen)
-        with open(f"{url_domain}.txt", 'a', encoding="utf-8") as data_file:
-            data_file.write("\n".join(seen))
 
+    end = time.perf_counter()
+    seen = sorted(crawler.seen)
+    print("Results:")
+
+    # for url in seen:
+    with open(url_domain + ".txt", 'a', encoding="utf-8") as f:
+        # with open("custom/"+url_domain+".txt", 'a', encoding="utf-8") as f:
+        f.write("\n".join(seen))
+
+    # Print summary of crawling
     print(f"Crawled: {len(crawler.done)} URLs")
     print(f"Found: {len(seen)} URLs")
-    print(f"Done in {time.perf_counter():.2f}s")
+    print(f"Done in {end - start:.2f}s")
 
 
 if __name__ == '__main__':
-    with open(FILE, "r", encoding="utf-8") as file:
-        urls = file.read().split("\n")
+    with open(FILE, "r", encoding="utf-8") as f:
+        urls = f.read().split("\n")
     for url in urls:
         asyncio.run(main(url), debug=True)
